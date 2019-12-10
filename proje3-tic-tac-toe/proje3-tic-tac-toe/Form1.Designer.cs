a44b6fc5a03a48d3859bb4f7c2a13e72553fb479
Muhammed KARADAŞ <31390903+MuhammedKaradas@users.noreply.github.com>
2019-12-10 12:27:12 +0300

    olusturulan-ilk-dosyalar-eklendi-add-source-code

---
+++ proje3-tic-tac-toe/proje3-tic-tac-toe/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace proje3_tic_tac_toe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.yeniOyunToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.çıkışToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(31, 116);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 82);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(130, 116);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(85, 82);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.Location = new System.Drawing.Point(228, 116);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(85, 82);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button4.Location = new System.Drawing.Point(31, 204);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(85, 82);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button5.Location = new System.Drawing.Point(130, 204);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(85, 82);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button6.Location = new System.Drawing.Point(228, 204);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(85, 82);
+            this.button6.TabIndex = 5;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button7.Location = new System.Drawing.Point(31, 292);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(85, 82);
+            this.button7.TabIndex = 6;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button8.Location = new System.Drawing.Point(130, 292);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(85, 82);
+            this.button8.TabIndex = 7;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button9.Location = new System.Drawing.Point(228, 292);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(85, 82);
+            this.button9.TabIndex = 8;
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(84, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(146, 20);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Sıradaki Oyuncu";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(248, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 20);
+            this.label2.TabIndex = 10;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.yeniOyunToolStripMenuItem,
+            this.çıkışToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(144, 52);
+            this.contextMenuStrip1.Text = "Dosya";
+            // 
+            // yeniOyunToolStripMenuItem
+            // 
+            this.yeniOyunToolStripMenuItem.Name = "yeniOyunToolStripMenuItem";
+            this.yeniOyunToolStripMenuItem.Size = new System.Drawing.Size(143, 24);
+            this.yeniOyunToolStripMenuItem.Text = "Yeni Oyun";
+            // 
+            // çıkışToolStripMenuItem
+            // 
+            this.çıkışToolStripMenuItem.Name = "çıkışToolStripMenuItem";
+            this.çıkışToolStripMenuItem.Size = new System.Drawing.Size(143, 24);
+            this.çıkışToolStripMenuItem.Text = "Çıkış";
+            this.çıkışToolStripMenuItem.Click += new System.EventHandler(this.çıkışToolStripMenuItem_Click);
+            // 
+            // button10
+            // 
+            this.button10.ContextMenuStrip = this.contextMenuStrip1;
+            this.button10.Location = new System.Drawing.Point(12, 3);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(104, 29);
+            this.button10.TabIndex = 12;
+            this.button10.Text = "Dosya";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(130, 3);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(104, 29);
+            this.button11.TabIndex = 13;
+            this.button11.Text = "Yardım";
+            this.button11.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(347, 399);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem yeniOyunToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem çıkışToolStripMenuItem;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+    }
+}
+
---
+++ proje3-tic-tac-toe/proje3-tic-tac-toe/Form1.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace proje3_tic_tac_toe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "Tic Tac Toe";
+            label2.Text = "X";
+        }
+
+        int xPlayer = 0, oPlayer = 0, xSayac = 0, oSayac = 0, oyuncuSirasi = 0;
+        int[] xKonum = new int[9];
+        int[] oKonum = new int[9];
+        string x = "X",o="O";
+        //string filePath= @"C:\\playerRank.txt";
+        //FileStream playerRank = new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.Write);
+
+        
+        FileStream fs = File.Create(@"C:playerRank.txt");//klasör içerisine dosya oluşturma 
+        StreamWriter playerRank = new StreamWriter("C:\\playerRank.txt");
+        public void kazanan()
+        {
+            /*if (1 ==)
+            {
+                
+            }*/
+            for (int i = 0; i < xKonum.Length; i++)
+            {
+                if (xKonum[0] == xKonum[3] && xKonum[3] == xKonum[6] && xKonum[0] == xKonum[6])//1
+                {
+                    //1-4-7
+                    MessageBox.Show("X1 Kazandı...");
+                    return;
+                }
+                else if(xKonum[1] == xKonum[4] && xKonum[4] == xKonum[7] && xKonum[1] == xKonum[7])//2
+                {
+                    //2-5-8
+                    MessageBox.Show("X2 Kazandı...");
+                    return;
+                }
+                else if (xKonum[2] == xKonum[5] && xKonum[5] == xKonum[8] && xKonum[2] == xKonum[8])//3
+                {
+                    //3-6-9
+                    MessageBox.Show("X3 Kazandı...");
+                    return;
+                }
+                else if (xKonum[0] == xKonum[4] && xKonum[4] == xKonum[8] && xKonum[0] == xKonum[8])//4
+                {
+                    //1-5-9
+                    MessageBox.Show("X4 Kazandı...");
+                    return;
+                }
+                else if (xKonum[2] == xKonum[4] && xKonum[4] == xKonum[6] && xKonum[2] == xKonum[6])//5
+                {
+                    //3-5-7
+                    MessageBox.Show("X5 Kazandı...");
+                    return;
+                }
+                else if (xKonum[0] == xKonum[1] && xKonum[1] == xKonum[2] && xKonum[0] == xKonum[2])//6
+                {
+                    //1-2-3
+                    MessageBox.Show("X6 Kazandı...");
+                    return;
+                }
+                else if (xKonum[3] == xKonum[4] && xKonum[4] == xKonum[5] && xKonum[3] == xKonum[5])//7
+                {
+                    //4-5-6
+                    MessageBox.Show("X7 Kazandı...");
+                    return;
+                }
+                else if (xKonum[6] == xKonum[7] && xKonum[7] == xKonum[8] && xKonum[6] == xKonum[8])//8
+                {
+                    //7-8-9
+                    MessageBox.Show("X8 Kazandı...");
+                    return;
+                }
+                else
+                {
+                    MessageBox.Show("Hata!");
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.Enabled = false;
+            if (oyuncuSirasi == 0)
+            {
+                button2.Text = x;
+                label2.Text = o;
+
+                //xKonum[1] = 2;
+                oyuncuSirasi++;
+                xSayac++;
+            }
+            else if (oyuncuSirasi==1)
+            {
+                button2.Text = o;
+                label2.Text = x;
+                oKonum[1] = 2;
+                oyuncuSirasi =0;
+                oSayac++;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button3.Enabled = false;
+            if (oyuncuSirasi == 0)
+            {
+                button3.Text = x;
+                label2.Text = o;
+                //xKonum[2] = 3;
+                oyuncuSirasi++;
+                xSayac++;
+            }
+            else if (oyuncuSirasi == 1)
+            {
+                button3.Text = o;
+                label2.Text = x;
+                oKonum[2] = 3;
+                oyuncuSirasi = 0;
+                oSayac++;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            button5.Enabled = false;
+            if (oyuncuSirasi == 0)
+            {
+                button5.Text = x;
+                label2.Text = o;
+                //xKonum[4] = 2;
+                oyuncuSirasi++;
+                xSayac++;
+            }
+            else if (oyuncuSirasi == 1)
+            {
+                button5.Text = o;
+                label2.Text = x;
+                oKonum[4] = 2;
+                oyuncuSirasi = 0;
+                oSayac++;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            button4.Enabled = false;
+            if (oyuncuSirasi == 0)
+            {
+                button4.Text = x;
+                label2.Text = o;
+                xKonum[3] = 1;
+                oyuncuSirasi++;
+                xSayac++;
+            }
+            else if (oyuncuSirasi == 1)
+            {
+                button4.Text = o;
+                label2.Text = x;
+                oKonum[3] = 32313;
+                oyuncuSirasi = 0;
+                oSayac++;
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            button9.Enabled = false;
+            if (oyuncuSirasi == 0)
+            {
+                button9.Text = x;
+                label2.Text = o;
+                oyuncuSirasi++;
+                xSayac++;
+            }
+            else if (oyuncuSirasi == 1)
+            {
+                button9.Text = o;
+                label2.Text = x;
+                oyuncuSirasi = 0;
+                oSayac++;
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            button7.Enabled = false;
+            if (oyuncuSirasi == 0)
+            {
+                button7.Text = x;
+                label2.Text = o;
+                xKonum[6] = 1;
+                oyuncuSirasi++;
+                xSayac++;
+            }
+            else if (oyuncuSirasi == 1)
+            {
+                button7.Text = o;
+                label2.Text = x;
+                oyuncuSirasi = 0;
+                oSayac++;
+                
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            button8.Enabled = false;
+            if (oyuncuSirasi == 0)
+            {
+                button8.Text = x;
+                label2.Text = o;
+                //xKonum[7] = 2;
+                oyuncuSirasi++;
+                xSayac++;
+            }
+            else if (oyuncuSirasi == 1)
+            {
+                button8.Text = o;
+                label2.Text = x;
+                oyuncuSirasi = 0;
+                oSayac++;
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            button6.Enabled = false;
+            if (oyuncuSirasi == 0)
+            {
+                button6.Text = x;
+                label2.Text = o;
+                oyuncuSirasi++;
+                xSayac++;
+            }
+            else if (oyuncuSirasi == 1)
+            {
+                button6.Text = o;
+                label2.Text = x;
+                oyuncuSirasi = 0;
+                oSayac++;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            if (oyuncuSirasi==0)
+            {
+                button1.Text = x;
+                label2.Text = o;
+                xKonum[0] = 1;
+                oyuncuSirasi++;
+                xSayac++;
+            }
+            else if (oyuncuSirasi == 1)
+            {
+                button1.Text = o;
+                label2.Text = x;
+                oKonum[0] = 1;
+                oyuncuSirasi = 0;
+               /* if (oSayac==2)
+                {
+                    MessageBox.Show("Sayac 3 O kazandi");
+                }*/
+               oSayac++;
+            }
+        }
+
+        private void çıkışToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
